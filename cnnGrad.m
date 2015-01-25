aa6218d32517383bfcdb9e30195ccc57e5af6b11
wuminye <huanlekf@163.com>
2015-01-25 14:43:30 +0800

    添加Conv梯度计算代码
    添加 Conv --> Pooling 梯度计算
    添加res.t域，表示误差项，独立于res.b域.

--- cnnGrad.m
+++ cnnGrad.m
@@ -3,18 +3,24 @@ function [ res ] = cnnGrad( model, data , y ,m )
 %   Detailed explanation goes here
 num = length(model.Layer);
 res = cell(num,1);
-res{num}.b = (data{num} - y)./m;
+
+%最后一层的误差值
+res{num}.t = (data{num} - y)./m;
+res{num}.b = res{num}.t;
 res{num}.w = (data{num} - y)*data{num-1}'./m;
+
 for i = num-1:-1: 2
     t = model.Layer{i};
     cur = t.type;
     nex =  model.Layer{i+1}.type;
     res{i}.b = 0;
+    res{i}.t = 0;
     res{i}.w = 0;
        
     
     if strcmp(cur,'Pooling') && strcmp(nex,'ANN')
-        res{i}.b = model.Layer{i+1}.w'*res{i+1}.b;  %.*(data{i}.*(1-data{i}));Pooling层没有激活函数
+        res{i}.t = model.Layer{i+1}.w'*res{i+1}.t;  %.*(data{i}.*(1-data{i}));Pooling层没有激活函数
+        res{i}.b = res{i}.t;
         res{i}.b = reshape(res{i}.b,1,1,size(res{i}.b,1));% 转化成featureMap
         %res{i}.w = res{i}.b*data{i-1}'; Pooling层没有权值
     end
@@ -23,29 +29,40 @@ for i = num-1:-1: 2
         k = model.Layer{i+1}.kernel;
         B = ones(size(k));
         %初始化误差featuremap
-        res{i}.b = zeros(size(model.Layer{i-1}.out));
+        res{i}.t = zeros(size(model.Layer{i-1}.out));
         %计算有效误差矩阵的大小
         x = size(res{i+1}.b,1)*size(k,1);
         y = size(res{i+1}.b,2)*size(k,2);
         
         for j = 1 : size(res{i+1}.b,3)
             %有效误差矩阵
-            res{i}.b(1:x,1:y,j) = kron(res{i+1}.b(:,:,j) , A);
+            res{i}.t(1:x,1:y,j) = kron(res{i+1}.b(:,:,j) , A);
         end
+        
         %计算卷积层的核函数梯度
         if strcmp(cur,'Conv')
             %Initialize Conv Kernel
-            res{i}.w = zeros(size(model.Layer{i}.k));
-            
-            for j = 1 : size(res{i}.w,3)
-           %     res{i}.w(:,:,j) = conv2(data{i-1}(:,:,))
+            res{i}.w = zeros(size(model.Layer{i}.w));
+            res{i}.b = zeros(size(model.Layer{i}.b));
+            for q = 1 : size(res{i}.w,4)
+               for p = 1 : size(res{i}.w,3)
+                   res{i}.w(:,:,p,q) = conv2(data{i-1}(:,:,p), rot90(rot90(res{i}.t(:,:,q))),'valid');
+                   %------------这里要不要再翻转回来呢?????????????????????
+                   %res{i}.w(:,:,p,q) = rot90(rot90(conv2(data{i-1}(:,:,p), rot90(rot90(res{i}.t(:,:,q))),'valid')));
+               end
+               tem =res{i}.t(:,:,q);
+               res{i}.b(q) = sum(tem(:));
             end
             
+            
+        else
+             res{i}.b =  res{i}.t; % 非卷积层
         end
     end
     
     if strcmp(cur,'ANN')
-        res{i}.b = model.Layer{i+1}.w'*res{i+1}.b.*(data{i}.*(1-data{i}));
+        res{i}.t = model.Layer{i+1}.w'*res{i+1}.t.*(data{i}.*(1-data{i}));
+        res{i}.b = res{i}.t;
         res{i}.w = res{i}.b*data{i-1}';
     end
 end
