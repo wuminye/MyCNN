e66e3400622b82860bd4efb1d614a6db06aafbb9
wuminye <huanlekf@163.com>
2015-01-26 14:37:10 +0800

    进一步FeatureMap规范化，添加梯度计算
    ANN层输出、梯度FeatureMap规范化。
    添加梯度计算:
    Conv --> ANN
    Conv --> Conv

--- cnnGrad.m
+++ cnnGrad.m
@@ -18,6 +18,52 @@ for i = num-1:-1: 2
     res{i}.b = 0;
     res{i}.t = 0;
     res{i}.w = 0;
+    if strcmp(cur,'Conv') && strcmp(nex,'Conv')
+        res{i}.t = zeros(model.Layer{i}.out);
+       
+        for p = 1:size(res{i}.t,3)
+            for q = 1:size(res{i+1}.t,3)
+               if model.Layer{i+1}.connector(q,p)~=1
+                   continue;
+               end
+               res{i}.t(:,:,p) = res{i}.t(:,:,p) + ...
+                        conv2(res{i+1}.t(:,:,q),model.Layer{i+1}.w(:,:,p,q),'full');
+            end
+        end  
+        %修正卷积层的误差，要乘以导数。
+        res{i}.t = res{i}.t.*(data{i}.*(1-data{i}));
+        
+        res{i}.w = zeros(size(model.Layer{i}.w));
+        res{i}.b = zeros(size(model.Layer{i}.b));
+        for q = 1 : size(res{i}.w,4)
+           for p = 1 : size(res{i}.w,3)
+               res{i}.w(:,:,p,q) = conv2(data{i-1}(:,:,p), rot90(rot90(res{i}.t(:,:,q))),'valid');
+               %------------这里要不要再翻转回来呢?????????????????????
+               %res{i}.w(:,:,p,q) = rot90(rot90(conv2(data{i-1}(:,:,p), rot90(rot90(res{i}.t(:,:,q))),'valid')));
+            end
+            tem =res{i}.t(:,:,q);
+            res{i}.b(q) = sum(tem(:));
+        end
+        
+    end
+    
+    if strcmp(cur,'Conv') && strcmp(nex,'ANN')
+        te = reshape(data{i}(1,1,:), [] ,1);
+        res{i}.t = model.Layer{i+1}.w'*reshape(res{i+1}.t,[],1).*(te.*(1-te));
+        res{i}.t = reshape(res{i}.t,1,1,[]);
+        
+        res{i}.w = zeros(size(model.Layer{i}.w));
+        res{i}.b = zeros(size(model.Layer{i}.b));
+        for q = 1 : size(res{i}.w,4)
+           for p = 1 : size(res{i}.w,3)
+               res{i}.w(:,:,p,q) = conv2(data{i-1}(:,:,p), rot90(rot90(res{i}.t(:,:,q))),'valid');
+               %------------这里要不要再翻转回来呢?????????????????????
+               %res{i}.w(:,:,p,q) = rot90(rot90(conv2(data{i-1}(:,:,p), rot90(rot90(res{i}.t(:,:,q))),'valid')));
+            end
+            tem =res{i}.t(:,:,q);
+            res{i}.b(q) = sum(tem(:));
+        end
+    end
       
     if strcmp(cur,'Pooling') && strcmp(nex,'Conv')   
        res{i}.t = zeros(model.Layer{i}.out);
@@ -35,7 +81,7 @@ for i = num-1:-1: 2
     end
     
     if strcmp(cur,'Pooling') && strcmp(nex,'ANN')
-        res{i}.t = model.Layer{i+1}.w'*res{i+1}.t;  %.*(data{i}.*(1-data{i}));Pooling层没有激活函数
+        res{i}.t = model.Layer{i+1}.w'*reshape(res{i+1}.t,[],1);  %.*(data{i}.*(1-data{i}));Pooling层没有激活函数
         res{i}.b = reshape(res{i}.t,1,1,[]);% 转化成featureMap
         res{i}.t = res{i}.b;
         %res{i}.w = res{i}.b*data{i-1}'; Pooling层没有权值
@@ -81,9 +127,12 @@ for i = num-1:-1: 2
     end
     
     if strcmp(cur,'ANN')
-        res{i}.t = model.Layer{i+1}.w'*res{i+1}.t.*(data{i}.*(1-data{i}));
+        te = reshape(data{i}(1,1,:), [] ,1);
+        res{i}.t = model.Layer{i+1}.w'*reshape(res{i+1}.t,[],1).*(te.*(1-te));
         res{i}.b = res{i}.t;
-        res{i}.w = res{i}.b*data{i-1}';
+        res{i}.t = reshape(res{i}.t,1,1,[]);
+        res{i}.w = res{i}.b*reshape(data{i-1}, [] ,1)';
+        res{i}.b = res{i}.t;
     end
 end
 
