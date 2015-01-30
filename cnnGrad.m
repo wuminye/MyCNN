f74a1e9afe1e13bf4a8f9815e4622b62519c12a3
wuminye <huanlekf@163.com>
2015-01-30 17:31:24 +0800

    梯度计算代码重构Beta
    重构梯度计算代码, 将误差矩阵和梯度的计算分离开，提高代码的复用率，更简练。

--- cnnGrad.m
+++ cnnGrad.m
@@ -29,7 +29,13 @@ for i = num-1:-1: 2
     res{i}.b = 0;
     res{i}.t = 0;
     res{i}.w = 0;
-    if strcmp(cur,'Conv') && strcmp(nex,'Conv')
+    
+%=====================误差矩阵计算=================================
+    if strcmp(nex,'Reshape')
+       res{i}.t = reshape(res{i+1}.t,model.Layer{i}.out);
+    end
+    
+    if strcmp(nex,'Conv')
         res{i}.t = zeros(model.Layer{i}.out);
        
         for p = 1:size(res{i}.t,3)
@@ -41,6 +47,36 @@ for i = num-1:-1: 2
                         conv2(res{i+1}.t(:,:,q),model.Layer{i+1}.w(:,:,p,q),'full');
             end
         end  
+    end
+    
+    
+    if  strcmp(nex,'ANN') || strcmp(nex,'SoftMax')
+        %te = reshape(data{i}(1,1,:), [] ,1);
+        res{i}.t = model.Layer{i+1}.w'*reshape(res{i+1}.t,[],1); %.*(te.*(1-te));
+        res{i}.t = reshape(res{i}.t,1,1,[]);
+    end
+    
+    if strcmp(nex,'Pooling')
+        k = model.Layer{i+1}.kernel;
+        B = ones(k.x,k.y);
+        %初始化误差featuremap
+        res{i}.t = zeros(model.Layer{i}.out);
+        %计算有效误差矩阵的大小
+        x = size(res{i+1}.t,1)*k.x;
+        y = size(res{i+1}.t,2)*k.y;
+        
+        for j = 1 : size(res{i+1}.t,3)
+            %有效误差矩阵
+            res{i}.t(1:x,1:y,j) = kron(res{i+1}.t(:,:,j) , B)./(k.x*k.y);
+            %res{i}.t(1:x,1:y,j) = kron(res{i+1}.b(:,:,j) , B);
+        end
+        
+    end
+    
+%================梯度计算================================================    
+     %计算卷积层的核函数梯度
+    if strcmp(cur,'Conv') 
+
         %修正卷积层的误差，要乘以导数。
         res{i}.t = res{i}.t.*(data{i}.*(1-data{i}));
         
@@ -57,94 +93,22 @@ for i = num-1:-1: 2
         end
         
     end
-    
-    if strcmp(cur,'Conv') && ( strcmp(nex,'ANN') || strcmp(nex,'SoftMax'))
-        te = reshape(data{i}(1,1,:), [] ,1);
-        res{i}.t = model.Layer{i+1}.w'*reshape(res{i+1}.t,[],1).*(te.*(1-te));
-        res{i}.t = reshape(res{i}.t,1,1,[]);
-        
-        res{i}.w = zeros(size(model.Layer{i}.w));
-        res{i}.b = zeros(size(model.Layer{i}.b));
-        for q = 1 : size(res{i}.w,4)
-           for p = 1 : size(res{i}.w,3)
-               res{i}.w(:,:,p,q) = conv2(data{i-1}(:,:,p), rot90(rot90(res{i}.t(:,:,q))),'valid');
-               %------------这里要不要再翻转回来呢?????????????????????
-               %res{i}.w(:,:,p,q) = rot90(rot90(conv2(data{i-1}(:,:,p), rot90(rot90(res{i}.t(:,:,q))),'valid')));
-            end
-            tem =res{i}.t(:,:,q);
-            res{i}.b(q) = sum(tem(:));
-        end
-    end
-      
-    if strcmp(cur,'Pooling') && strcmp(nex,'Conv')   
-       res{i}.t = zeros(model.Layer{i}.out);
        
-       for p = 1:size(res{i}.t,3)
-           for q = 1:size(res{i+1}.t,3)
-               if model.Layer{i+1}.connector(q,p)~=1
-                   continue;
-               end
-               res{i}.t(:,:,p) = res{i}.t(:,:,p) + ...
-                        conv2(res{i+1}.t(:,:,q),model.Layer{i+1}.w(:,:,p,q),'full');
-           end
-        end 
-       
+    if strcmp(cur,'Pooling') 
+ 
+       % res{i}.b = res{i}.t ; %.*(data{i}.*(1-data{i}));Pooling层没有激活函数
+
     end
     
-    if strcmp(cur,'Pooling') && (strcmp(nex,'ANN') || strcmp(nex,'SoftMax'))
-        res{i}.t = model.Layer{i+1}.w'*reshape(res{i+1}.t,[],1);  %.*(data{i}.*(1-data{i}));Pooling层没有激活函数
-        res{i}.b = reshape(res{i}.t,1,1,[]);% 转化成featureMap
-        res{i}.t = res{i}.b;
-        %res{i}.w = res{i}.b*data{i-1}'; Pooling层没有权值
-    end
-    
-    if (strcmp(cur,'Pooling') || strcmp(cur,'Conv')) && strcmp(nex,'Pooling')
-        k = model.Layer{i+1}.kernel;
-        B = ones(k.x,k.y);
-        %初始化误差featuremap
-        res{i}.t = zeros(model.Layer{i}.out);
-        %计算有效误差矩阵的大小
-        x = size(res{i+1}.t,1)*k.x;
-        y = size(res{i+1}.t,2)*k.y;
-        
-        for j = 1 : size(res{i+1}.t,3)
-            %有效误差矩阵
-            res{i}.t(1:x,1:y,j) = kron(res{i+1}.t(:,:,j) , B)./(k.x*k.y);
-            %res{i}.t(1:x,1:y,j) = kron(res{i+1}.b(:,:,j) , B);
-        end
-        
-        %计算卷积层的核函数梯度
-        if strcmp(cur,'Conv')
-            
-            %修正卷积层的误差，要乘以导数。
-            res{i}.t = res{i}.t.*(data{i}.*(1-data{i}));
-            
-            %Initialize Conv Kernel
-            res{i}.w = zeros(size(model.Layer{i}.w));
-            res{i}.b = zeros(size(model.Layer{i}.b));
-            for q = 1 : size(res{i}.w,4)
-               for p = 1 : size(res{i}.w,3)
-                   res{i}.w(:,:,p,q) = conv2(data{i-1}(:,:,p), rot90(rot90(res{i}.t(:,:,q))),'valid');
-                   %------------这里要不要再翻转回来呢?????????????????????
-                   %res{i}.w(:,:,p,q) = rot90(rot90(conv2(data{i-1}(:,:,p), rot90(rot90(res{i}.t(:,:,q))),'valid')));
-               end
-               tem =res{i}.t(:,:,q);
-               res{i}.b(q) = sum(tem(:));
-            end
-            
-        else
-             res{i}.b =  res{i}.t; % 非卷积层
-        end
-    end
     
     if strcmp(cur,'ANN')
-        te = reshape(data{i}(1,1,:), [] ,1);
-        res{i}.t = model.Layer{i+1}.w'*reshape(res{i+1}.t,[],1).*(te.*(1-te));
+        res{i}.t = res{i}.t.*(data{i}.*(1-data{i}));
         res{i}.b = res{i}.t;
-        res{i}.t = reshape(res{i}.t,1,1,[]);
+        res{i}.t = reshape(res{i}.t,1,1,[]); % 可要可不要？？
         res{i}.w = res{i}.b*reshape(data{i-1}, [] ,1)';
         res{i}.b = res{i}.t;
     end
+%============================================================================    
 end
 
 end
