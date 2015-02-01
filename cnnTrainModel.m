96e5a0e3dc992fea2f268e62916b2e62a5fc1524
wuminye <huanlekf@163.com>
2015-02-01 19:05:49 +0800

    SGD Beta 2.0
    根据新对SGD的理解写出的SGD训练代码，执行main2

--- cnnTrainModel.m
+++ cnnTrainModel.m
@@ -2,11 +2,19 @@ function [ outmodel ] = cnnTrainModel( model, X , y , step, ng ,lambda )
 %CNNTRAINMODEL Summary of this function goes here
 %   Detailed explanation goes here
 theta = SaveTheta(model);
+num_train = size(X,4);
+%pn = ceil(num_train./150); % 随机取的样本个数
+pn=24;
 for i = 1: step
-   [ J, grad ] = CostFunctionSGD( theta, X , y, model , lambda);
-   fprintf('%e\n',mean(grad));
-   theta = theta - ng*grad;
+  fprintf('============ %d =============%d\n',i,pn);
+   index = randperm(num_train,pn); 
+   F=@(p)CostFunction( p, X(:,:,:,index), y(index), model ,0.01);
+  %[ J, grad ] = CostFunctionSGD( theta, X , y, model , lambda);
+  options = optimset('MaxIter', 30);
+  [nn_params, cost] = fmincg(F, theta, options);
+  theta = nn_params;
 end
+model = LoadTheta(theta,model);
 outmodel = model;
 
 end
