0a4d71d3a93ad1f0894ff48a1c5589b1e23d4bde
wuminye <huanlekf@163.com>
2015-02-01 15:19:55 +0800

    修正SGD的bug
    修正SGD的bug ，独立出SGD的CostFunction,   使用FSGD函数句柄来训练

---
+++ CostFunctionSGD.m
@@ -0,0 +1,11 @@
+function [ J, grad ] = CostFunctionSGD( theta , input , y, model ,lambda)
+%COSTFUNCTIONSGD Summary of this function goes here
+%   Detailed explanation goes here
+num_train = size(input,4);
+pn = ceil(num_train./2); % 随机取的样本个数
+
+index = randperm(num_train,pn);
+
+[ J, grad ] = CostFunction( theta , input(:,:,:,index) , y(index), model ,lambda);
+end
+
--- main.m
+++ main.m
@@ -1,5 +1,5 @@
 tic;
-num_train = 10000;
+num_train = 10;
 
 imageDim = 28;
 images = loadMNISTImages('train-images.idx3-ubyte');
@@ -12,14 +12,15 @@ labels = labels(1:num_train);
 %images = reshape(images,784,1,size(images,3));
 model = GetModel([28 28 1]);
 
-pn = ceil(num_train./20); % 随机取的样本个数
+
 
 F=@(p)CostFunction( p, images, labels, model ,0.01);
+FSGD=@(p)CostFunctionSGD( p, images, labels, model ,0.01);
 %FF = @(p)checkcf(p,squeeze(images)',labels(1:num_train),0.01,[784 81 10]');
 theta = SaveTheta(model);
 fprintf('Start training....\n');
 options = optimset('MaxIter', 450);
-[nn_params, cost] = fmincg(F, theta, options);
+[nn_params, cost] = fmincg(FSGD, theta, options);
 model = LoadTheta(nn_params,model);
 save model  model
 toc;
