5309dee5df60a7338ca22f7f2192f6387cca2efc
wuminye <huanlekf@163.com>
2015-02-09 20:55:10 +0800

    添加人脸区域热度图的计算和可视化
    添加人脸区域热度图的计算和可视化

---
+++ FaceData/CalcGuass.m
@@ -0,0 +1,23 @@
+function [ Z ] = CalcGuass( data ,  scale )
+%CALCGUASS Summary of this function goes here
+%   Detailed explanation goes here
+imsize = [data.img_width  data.img_height];
+Z = zeros(flip(imsize/scale));
+x = 1:size(Z,2);
+y = 1:size(Z,1);
+[xx,yy] = meshgrid(x,y);
+
+for i = 1 : length(data.face)
+   t = data.face{i}.position;
+   
+   th = (t.height*data.img_height+t.width*data.img_width)/(800*scale);
+   
+   zz = exp(-( (xx-data.img_width*t.center.x/scale/100).^2 + (yy-t.center.y*data.img_height/scale/100).^2 )/(2*th.^2));
+   zz(zz<0.1) = 0;
+   zz=zz*1.4;
+   zz(zz>0.95)=1;
+   Z = Z + zz;
+end
+
+end
+
---
+++ FaceData/Showimg.m
@@ -0,0 +1,47 @@
+function [  ] = Showimg(data,dir)
+%SHOWIMG Summary of this function goes here
+%   Detailed explanation goes here
+close all;
+scale = 10;
+
+F = imread([dir data.filename]);
+imshow(F);
+hold on;
+rst = data.data;
+face = rst{1}.face;
+img_width = rst{1}.img_width;
+img_height = rst{1}.img_height;
+for k = 1 : length(face)
+         % Draw face rectangle on the image
+        face_i = face{k};
+        center = face_i.position.center;
+        w = face_i.position.width / 100 * img_width;
+        h = face_i.position.height / 100 * img_height;
+        rectangle('Position', ...
+            [center.x * img_width / 100 -  w/2, center.y * img_height / 100 - h/2, w, h], ...
+            'Curvature', 0.4, 'LineWidth',2, 'EdgeColor', 'blue');
+        % Detect facial key points
+        ps = face_i.position;
+        pt = ps.eye_left;
+        scatter(pt.x * img_width / 100, pt.y * img_height / 100, 'g.');
+        
+        pt = ps.eye_right;
+        scatter(pt.x * img_width / 100, pt.y * img_height / 100, 'g.');
+        
+        pt = ps.mouth_left;
+        scatter(pt.x * img_width / 100, pt.y * img_height / 100, 'g.');
+        
+        pt = ps.mouth_right;
+        scatter(pt.x * img_width / 100, pt.y * img_height / 100, 'g.');
+        
+        pt = ps.nose;
+        scatter(pt.x * img_width / 100, pt.y * img_height / 100, 'g.');
+end
+
+Z = CalcGuass(data.data{1}, scale);
+figure;
+imshow(double(imresize(F,1/scale)).*Z/255);
+figure;
+mesh(Z);
+end
+
