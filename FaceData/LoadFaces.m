23bade0d35ed78c8c25c3e091dc6a584589aa3e7
wuminye <huanlekf@163.com>
2015-02-13 14:49:43 +0800

    人脸样本读取
    读取36*32的人脸样本，并添加中值滤波后的样本

--- FaceData/LoadFaces.m
+++ FaceData/LoadFaces.m
@@ -1,72 +1,58 @@
-function [ X,yx,yy] = LoadFaces()
-%LOADFACES Summary of this function goes here
+function [ X,y] = LoadFaces()
+% 加载人脸样本
 %   Detailed explanation goes here
   load('Database');
-  dir = './FaceData/pic/';
-  cy = 16;
-  cx = 12;
-  step = 5;
-  scale = 6;
-  rx = 120;
-  ry = 160;
-  dx = rx / 12;
-  dy = ry / 16;
+  %dir = './FaceData/pic/';
+ dir = './pic/';
+ 
+  rx = 36; %目标高度
+  ry = 32; %目标宽度
+  
   N = Database.cnt ;
-  %N = 2 ;
+  N = 100 ;
   
   X = zeros(rx,ry,1,0);
-  yy = zeros(0,cy+1);
-  yx = zeros(0,cx+1);
+  y = zeros(0,2);
+ 
   fprintf('begin read IMG...\n');
   for i = 1 : N
-      if rand >0.29
+      data = Database.data{i};
+      ang = str2double(data.filename(6:8));
+      %大角度样本丢弃
+      if abs(ang)>=40
           continue;
       end
       fprintf('\r%5d\r',i);
-      data = Database.data{i};
-      [px,py] = getpoint(data.data{1});
-      px = px./(scale);
-      py = py./(scale);
-      F = imread([dir data.filename]);
-      F = double(imresize(F,1/scale))/255;
-      [sx sy] = size(F);
-      for p = 1:step:rx-sx
-          for q = 1:step:ry-sy
-            if rand >0.9
-             X(:,:,1,end+1) = rand(rx,ry,1);
-             X(p:p+sx-1,q:q+sy-1,1,end) = F;
-          %   subplot(2,2,1);
-           %  imshow(X(:,:,1,end));
-           %  hold on;
-          %   scatter(px+q-1,py+p-1, 'g.');
-             yx(end+1,ceil((py+p-1)/dy)+1) = 1;
-             yy(end+1,ceil((px+q-1)/dx)+1) = 1;
-           %  subplot(2,2,2);
-            % imagesc(yx(end,:)',[0 1]);
-            % subplot(2,2,3);
-            % imagesc(yy(end,:),[0 1]);
-            
-            %反样本
-             if rand<0.1
-                 X(:,:,1,end+1) = rand(rx,ry,1);
-                 X(p:p+sx-1,q:q+sy-1,1,end) = F(1);
-                 yx(end+1,1) = 1;
-                 yy(end+1,1) = 1;
-             end
-            end
-          end
-      end
       
+      [px,py,h,w] = getpoint(data.data{1});
+       w = w*1.15;
+      if abs(ang)>=25
+           w = w *1.1;
+      end
+ 
+      h = w*rx/ry;
+
+      F = imread([dir data.filename]);
+      F = double(F)/255;
+
+      nF = F(ceil(py-h/2:py+h/2),ceil(px - w/2:px + w/2));
+      nF = imresize(nF,[rx,ry]);
+      X(:,:,1,end+1) = nF;
+      y(end+1,1) = 1;
+      X(:,:,1,end+1) = medfilt2(nF);
+      y(end+1,1) = 1;
+      imshow(nF);
   end
   
   
 end
 
 
-function [px,py] = getpoint(data)
+function [px,py,h,w] = getpoint(data)
 
 t = data.face{1}.position;
 px = data.img_width*t.center.x/100;
 py = t.center.y*data.img_height/100;
-
+h = t.height*data.img_height/100;
+w = t.width*data.img_width/100;
 end
