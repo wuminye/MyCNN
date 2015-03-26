8c9184e7a8e726cb1a3df8c1417da5031a267935
wuminye <huanlekf@163.com>
2015-03-26 11:47:28 +0800

    修复梯度计算bug
    修复梯度计算bug,将Convs梯度编入梯度向量

--- CostFunction.m
+++ CostFunction.m
@@ -44,7 +44,8 @@ fs = T{1};
 % 合并多个样本的梯度
 for i = 2 : num_data
     for j = 1: length(T{i})
-       if strcmp(model.Layer{j}.type,'ANN') || strcmp(model.Layer{j}.type,'Conv')
+       if strcmp(model.Layer{j}.type,'ANN') || strcmp(model.Layer{j}.type,'Conv') ...
+               || strcmp(model.Layer{j}.type,'Convs')
            fs{j}.w = fs{j}.w + T{i}{j}.w;
            fs{j}.b = fs{j}.b + T{i}{j}.b;
        end
@@ -58,14 +59,15 @@ fprintf('%.5f%%  %e \r',cor,J);
 %计算正则项梯度偏差
 for j = 1: length(model.Layer)
    if strcmp(model.Layer{j}.type,'ANN') || strcmp(model.Layer{j}.type,'Conv') ...
-           || strcmp(model.Layer{j}.type,'SoftMax')
+           || strcmp(model.Layer{j}.type,'SoftMax') || strcmp(model.Layer{j}.type,'Convs')
       fs{j}.w = fs{j}.w + lambda*model.Layer{j}.w./num_data;
    end
 end
 
 %生成梯度向量
 for i = 1 : length(fs)
-    if strcmp(model.Layer{i}.type,'ANN') || strcmp(model.Layer{i}.type,'Conv') 
+    if strcmp(model.Layer{i}.type,'ANN') || strcmp(model.Layer{i}.type,'Conv') ...
+            || strcmp(model.Layer{i}.type,'Convs')
         grad = [grad;fs{i}.b(:);fs{i}.w(:);];
     end
     if strcmp(model.Layer{i}.type,'SoftMax')
