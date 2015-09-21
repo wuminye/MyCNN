730e3c670cc6c8fa72eff85739b9679627e272ec
wuminye <huanlekf@163.com>
2015-09-21 16:46:23 +0800

    Add dropout switch for ReTrain
    Add dropout switch for ReTrain

--- ReTrain.m
+++ ReTrain.m
@@ -4,7 +4,7 @@ addpath('./Util/');
 
 pn = input('the number of training : ');
 itn = input('the number of iteration : ');
-
+dropout = input('Enable Dropout? : ');
 load model
 
 
@@ -21,9 +21,13 @@ tic;
   model=cnnLog(model,'faces:%d\n',sum(ty(:,1)==1));
   options = optimset('MaxIter', itn);
   F=@(p)CostFunction( p, tX, ty, model );
+  if dropout == 1
    model = DropoutStart(model);
+  end
   [nn_params, cost , model ,corind] = fmincg(model,F, theta, options);
-  model = DropoutEnd(model);
+  if dropout == 1
+     model = DropoutEnd(model);
+  end
   model = LoadNetTheta(nn_params,model);
   
   
